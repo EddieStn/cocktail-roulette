d4f571d7710a48439cdb9c7643cb82d190cf8985
EddieStn <eduard.eduard25@yahoo.com>
2022-07-12 18:57:46 +0000

    Build the spin function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
 function spinTheWheel() {
+    let x = 1000; // min value
+    let y = 9000; // max value
+
+    let deg = Math.floor(Math.random() * (x - y)) + y;
+
+    document.getElementById("inner-box").style.transform = "rotate("+deg+"deg)";
 
 }
 
